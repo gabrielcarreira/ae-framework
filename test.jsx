c37fdefbcc38c0de02fcf24cfdbb439886ce07db
Gabriel Carreira <gabriel.carreira1@gmail.com>
2023-02-03 21:43:32 -0300

    prettier aplicado

--- test.jsx
+++ test.jsx
@@ -1,16 +1,16 @@
 //@include 'framework.jsx'
-
+ 
 app.beginUndoGroup('Framework Test')
 testLib()
 app.endUndoGroup()
 
 function testLib() {
   const frame = new getFrame()
-  
+
   changeAnchorPoint(frame.select, 0, 0)
   //applyFFXExpression('test.ffx', 'wiggle(1, effect("Test (Name)")(1))')
-  setProperty(frame.sel, 'pos.expression', 'wiggle(1, 20)')
-  setProperty(frame.sel, 'rot.expression', 'wiggle(1, 10)')
+  setProperty(frame.sel, 'pos.expression', 'wiggle(0.5, 10)')
+  setProperty(frame.sel, 'rot.expression', 'wiggle(0.5, 5)')
 
   setMethod(frame.sel, 'sca', 'setValueAtTime', [0, [0, 0]])
   setMethod(frame.sel, 'sca', 'setValueAtTime', [1, [100, 100]])
@@ -21,5 +21,5 @@ function testLib() {
   setMethod(frame.sel, 'sca', 'setTemporalEaseAtKey', [2, easeScale, easeScale])
   setMethod(frame.sel, 'rot', 'setTemporalEaseAtKey', [2, [ease], [ease]])
 
-  offsetLayers(frame.select, 2, 'random')
+  offsetLayers(frame.select, 6, 'convex')
 }
